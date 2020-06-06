3868b02d6f7558f10e04ba7c4ff683c5603c948d
zongzi531 <zongzi.xy@gmail.com>
2020-06-06 13:56:19 +0800

    feat: add signale, dotenv

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const chalk = require('chalk')
 const ora = require('ora')
+const signale = require('signale')
+const dotenv = require('dotenv')
 
 console.log(chalk.yellow('假装准备加载'))
 
@@ -8,4 +10,8 @@ const spinner = ora('假装加载中……').start()
 setTimeout(() => {
   spinner.stop()
   console.log(chalk.bgRed('假装加载完毕'))
+  console.log(chalk.bgWhite(JSON.stringify(dotenv.config())))
+  
 }, 3000)
+
+signale.fatal(new Error('报错咯!'))
